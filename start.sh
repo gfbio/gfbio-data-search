9c7e6c59754cf181a8a983fbfe1b2402bf431300
ctpfaff <Claas-Thido.Pfaff@gfbio.org>
2022-09-01 09:48:35 +0200

    Its DaiSi

--- start.sh
+++ start.sh
@@ -5,18 +5,18 @@ sed -i 's/class="toolbarMenu"/class="toolbarMenu" style="display:none;"/g' Datas
 
 # install and build
 # NG_CLI_ANALYTICS=ci will not ask if you want to participate in angular analitics during package installation
-cd DatasetSearchUI/angular/ && NG_CLI_ANALYTICS=ci npm i && npm run build && cd ../../
+cd DatasetSearchUI/angular/ && NG_CLI_ANALYTICS=ci npm i --legacy-peer-deps && npm run build && cd ../../
 
 # build our own react app
 cd search-ui && npm i && npm run build && cd ../
 
 # copy files from angular
 mkdir -p search-ui/build/static/js/angular
-cp DatasetSearchUI/angular/dist/DatasetSearch/*.js search-ui/build/static/js/angular/
-cp DatasetSearchUI/angular/dist/DatasetSearch/*.js.map search-ui/build/static/js/angular/
+cp DatasetSearchUI/angular/dist/DaiSi/*.js search-ui/build/static/js/angular/
+cp DatasetSearchUI/angular/dist/DaiSi/*.js.map search-ui/build/static/js/angular/
 
 mkdir -p search-ui/build/assets/img
-cp DatasetSearchUI/angular/dist/DatasetSearch/assets/img/* search-ui/build/assets/img/
+cp DatasetSearchUI/angular/dist/DaiSi/assets/img/* search-ui/build/assets/img/
 
 # clean submodule folder
 cd DatasetSearchUI && git reset --hard && git clean -f -d && cd ../
