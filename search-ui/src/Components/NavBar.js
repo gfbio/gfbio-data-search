e8827059b44a908d63e7cbf7829f4f8010b65bea
ctpfaff <Claas-Thido.Pfaff@gfbio.org>
2022-09-01 09:47:21 +0200

    Brings in logo

--- search-ui/src/Components/NavBar.js
+++ search-ui/src/Components/NavBar.js
@@ -1,26 +1,26 @@
-import logo from "../images/GFBio_logo.png";
+import logo from "../images/NFDI_logo_claim.png";
 
 export default function NavBar(props) {
-    const classes = {
-        navWrapper: {
-            maxWidth: '100%',
-            'box-shadow': '0 6px 6px -6px #999',
-            'background-color': 'white',
-        },
-        navBar: {
-            'max-width': '960px',
-            margin: '0 auto',
-            padding: '5px 0',
-        },
-        logo: {
-            height: '50px',
-        }
-    }
-    return (
-        <div style={classes.navWrapper}>
-            <div style={classes.navBar}>
-                <img src={logo} style={classes.logo}/>
-            </div>
-        </div>
-    )
+  const classes = {
+    navWrapper: {
+      maxWidth: "100%",
+      "box-shadow": "0 6px 6px -6px #999",
+      "background-color": "white",
+    },
+    navBar: {
+      "max-width": "960px",
+      margin: "0 auto",
+      padding: "5px 0",
+    },
+    logo: {
+      height: "50px",
+    },
+  };
+  return (
+    <div style={classes.navWrapper}>
+      <div style={classes.navBar}>
+        <img src={logo} style={classes.logo} />
+      </div>
+    </div>
+  );
 }
