562b94b57fff9ce9788dda752a2d515e7dcf2e87
Linus Franz <lfranz@gfbio.org>
2022-10-07 17:09:20 +0200

    DASS-146 Added Header and Footer - imported icons - imported fonts - imported scss - imported the html - changed the header - added the footer - add images

--- search-ui/src/index.js
+++ search-ui/src/index.js
@@ -1,8 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import NavBar from "./Components/NavBar";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import NavBar from './Components/NavBar';
+import Footer from './Components/Footer';
+import './scss/styles.scss'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +14,12 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+ReactDOM.render(
+  <React.StrictMode>
+    <Footer />
+  </React.StrictMode>,
+  document.getElementById('footer')
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
