cb277f0dd754a7058e353712603a93b81b0a0094
Linus Franz <lfranz@gfbio.org>
2022-10-07 17:09:20 +0200

    DASS-146 Added Header and Footer - imported icons - imported fonts - imported scss - imported the html - changed the header - added the footer - add images

--- search-ui/src/Components/NavBar.js
+++ search-ui/src/Components/NavBar.js
@@ -1,27 +1,48 @@
-// import logo from "../images/NFDI_logo_claim.png";
-import logo from "../images/GFBio_logo.png";
+import logo from "../images/gfbio_logo_small.svg";
+import React from "react";
 
 export default function NavBar(props) {
-  const classes = {
-    navWrapper: {
-      maxWidth: "100%",
-      boxShadow: "0 6px 6px -6px #999",
-      backgroundColor: "white",
-    },
-    navBar: {
-      maxWidth: "960px",
-      margin: "0 auto",
-      padding: "5px 0",
-    },
-    logo: {
-      height: "50px",
-    },
-  };
-  return (
-    <div style={classes.navWrapper}>
-      <div style={classes.navBar}>
-        <img src={logo} style={classes.logo} alt="NFDI logo with claim" />
-      </div>
-    </div>
-  );
+
+    const [navCollapsed, setNavCollapsed] = React.useState(true);
+
+    function toogleNav() {
+        setNavCollapsed(!navCollapsed);
+    }
+
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink navbar-shrink-content"
+        id="mainNavContent">
+            <div class="container">
+                <a class="navbar-brand " href="/" style={{ background:`url(${logo}) no-repeat center center` }}>
+                    <h1></h1>
+                </a>
+                <button class="navbar-toggler" type="button" onClick={toogleNav}>
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div className={"collapse navbar-collapse d-lg-flex flex-lg-row-reverse " + (navCollapsed ? "" : "show")}
+                    id="navbarResponsive" >
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item {% if request.path == url %}active{% endif %}">
+                            <a class="nav-link" href="https://gfbio.org/services/" target="_blank"
+                                    rel="noopener noreferrer">
+                                GFBio Services
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="https://gfbio.org/gfbio_ev">
+                                GFBio e.V.
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.nfdi4biodiversity.org/">
+                            NFDI 4 Bio <i class="mdi mdi-open-in-new"></i>
+                            </a>
+                        </li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
 }
