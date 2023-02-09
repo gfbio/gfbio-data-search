f5df23eb34558be61eee3a3f9419a9d4a264bdbb
Marc Weber <mweber@gfbio.org>
2023-02-09 10:41:19 +0100

    NFDI4BIO-238 add example.env in project root
    - change url in .gitlab-ci.yml release block
    - add .env to gitignore, since it now contains secrets

--- DatasetSearchUIOwn/angular/src/app/global.constants.ts
+++ DatasetSearchUIOwn/angular/src/app/global.constants.ts
@@ -1,4 +1,3 @@
 export class SearchSettings {
-    public static COLLECTIONS_API_ENDPOINT = 'https://collections.gfbio.dev/api/collections/';
-    public static COLLECTIONS_API_TOKEN = '5e181330380cab5cf6d8bb826003cbec2803cc22';
+    // public static COLLECTIONS_API_ENDPOINT = 'https://collections.gfbio.dev/api/collections/';
 }
