97779b7ffe55ead0343cdf9cbc395c5b65a0dab8
Marc Weber <mweber@gfbio.org>
2023-02-17 15:48:09 +0100

    DASS-521 add conditional to show or hide vat button
    - add method to access env variable and expose to template

--- DatasetSearchUIOwn/angular/src/environments/gfbio.environment.ts
+++ DatasetSearchUIOwn/angular/src/environments/gfbio.environment.ts
@@ -3,6 +3,6 @@ export const gfbioEnvironment = {
     COLLECTIONS_API_URL: 'https://collections.gfbio.dev/api/collections/',
     // TODO: add a specific API-Token here when testing and using ng-serve. This seems to be the better solution
     //  than automatically set a secret in a file that is under version control. Do not forget to remove
-    //  before commiting
+    //  before committing
     COLLECTIONS_API_TOKEN: 'API_TOKEN_HERE'
 };
