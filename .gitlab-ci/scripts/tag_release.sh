ba1bd87e05aac5d157ad69ef340f2eced4368b18
ctpfaff <Claas-Thido.Pfaff@gfbio.org>
2024-02-07 10:00:16 +0100

    Fixes name of compose file in script
    * we use production not staging

--- .gitlab-ci/scripts/tag_release.sh
+++ .gitlab-ci/scripts/tag_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp /home/gitlab-runner/.search-gfbio.node.env ./search/backend/.env
 cp /home/gitlab-runner/.environment.prod.ts ./search/frontend/src/environments/environment.prod.ts
-docker-compose -f staging.yml build --no-cache --force-rm
-docker-compose -f staging.yml down
-docker-compose -f staging.yml up -d
+docker-compose -f production.yml build --no-cache --force-rm
+docker-compose -f production.yml down
+docker-compose -f production.yml up -d
