fdf66eb9930136cca3b31814800a1966d4574f9e
ctpfaff <Claas-Thido.Pfaff@gfbio.org>
2024-03-07 10:47:07 +0100

    Migrate to production
    * Provides changes to ci pipline, the stages and tags for
      traefik

--- .gitlab-ci/scripts/tag_release.sh
+++ .gitlab-ci/scripts/tag_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp /home/gitlab-runner/.search-gfbio.node.env ./search/backend/.env
-cp /home/gitlab-runner/.environment.prod.ts ./search/frontend/src/environments/environment.prod.ts
+cp /home/gitlab-runner/environments/search.gfbio.org/search.gfbio.org.backend.env ./search/backend/.env
+cp /home/gitlab-runner/environments/search.gfbio.org/search.gfbio.org.frontend.env ./search/frontend/src/environments/environment.prod.ts
 docker-compose -f production.yml build --no-cache --force-rm
 docker-compose -f production.yml down
 docker-compose -f production.yml up -d
