50e2b6ead77882728a1124f51ebe2f7732ecc0f8
ctpfaff <Claas-Thido.Pfaff@gfbio.org>
2024-11-19 11:57:12 +0100

    Touches up styling of the citation dialog

--- search/frontend/src/app/citation/citation.component.ts
+++ search/frontend/src/app/citation/citation.component.ts
@@ -1,18 +1,38 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {Citation} from '../models/result/citation';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
-import {Hit} from '../models/result/hit';
+import { Component, Inject, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Citation } from '../models/result/citation';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Hit } from '../models/result/hit';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
     selector: 'app-citation',
     templateUrl: './citation.component.html',
-    styleUrls: ['./citation.component.css']
+    styleUrls: ['./citation.component.scss']
 })
 export class CitationComponent implements OnInit {
     result: Citation;
+    @ViewChild('citationText') citationText: ElementRef;
 
-    constructor(@Inject(MAT_DIALOG_DATA) public data: Hit) {
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: Hit,
+        private dialogRef: MatDialogRef<CitationComponent>,
+        private snackBar: MatSnackBar
+    ) {
+        this.dialogRef.addPanelClass('citation-dialog');
     }
+
     ngOnInit(): void {
         this.result = this.data.getCitation();
+    }
+
+    copyToClipboard(): void {
+        const text = this.citationText.nativeElement.innerText;
+        navigator.clipboard.writeText(text).then(() => {
+            this.snackBar.open('Citation copied to clipboard', 'Close', {
+                duration: 3000,
+                horizontalPosition: 'center',
+                verticalPosition: 'bottom'
+            });
+        });
     }
 }
